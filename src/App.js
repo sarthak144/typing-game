580b8edbcfea5f69c8f312a52a255598ae37b88e
Sarthak <sarthak@sheroes.in>
2022-07-16 01:44:34 +0530

    First Iteration

--- src/App.js
+++ src/App.js
@@ -1,37 +1,96 @@
 import {useState, useEffect} from 'react'
 var allAlphabets = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-// const SECONDS = 60
+const SECONDS = 0
 
 function App() {
   const [alphabets, setAlphabets] = useState([])
+  const [timer, setTimer] = useState(SECONDS)
+  const [index, setIndex] = useState(0)
+  const [currAlphabet, setCurrAlphabet] = useState()
+  const [currInput, setCurrInput] = useState("")
+  const [win, setWin] = useState(false)
+  const [finalTime, setFinalTime] = useState(0)
 
 
-
-  useEffect(() => {
-    setAlphabets(generateAlphabets())
-  }, [])
+  // useEffect(() => {
+  //   setAlphabets(generateAlphabets())
+  // }, [])
 
   function generateAlphabets() {
     var randomAlphabets = []
     for (var i = 0; i < 20; i++) {
-      randomAlphabets.push(allAlphabets[Math.floor(Math.random()*alphabets.length)]);
+      randomAlphabets.push(allAlphabets[Math.floor(Math.random()*allAlphabets.length)]);
+      if (i===0) {
+        setCurrAlphabet(randomAlphabets[0])
+      }
     }
 
     return randomAlphabets;
   }
 
+  function start()
+  { 
+    setAlphabets(generateAlphabets())
+    
+    let interval= setInterval(() => {
+      setTimer((prevTimer) => {
+      
+      return prevTimer+1
+    })
+    }
+    , 1000)
+  }
+
+  function handleKeyDown(event)
+  {
+    console.log(event.key)
+    if (event.key.toUpperCase()===alphabets[index])
+    {
+      setIndex(index+1)
+      setCurrAlphabet(alphabets[index+1])
+      console.log(index)
+      if (index===19)
+      {
+        setWin(true)
+        setFinalTime(timer)
+      }
+    }
+    else
+    {
+      setTimer((prevTimer) => {
+      
+        return prevTimer+0.5
+      })
+    }
+  }
 
 
 
   return (
     <div className="App">
       <div className="section">
+        <div className="is-size-1 has-text-centered has-text-primary">
+          <h2>{timer}</h2>
+        </div>
+      </div>
+      <div className="control is-expanded section">
+        <input  type="text" className="input"   onKeyDown={handleKeyDown} value={currInput} onChange={(e) => setCurrInput(e.target.value)}/>
+      </div>
+      <div className="section">
+        <button className="button is-info is-fullwidth" onClick={start} >
+          Start
+        </button>
+      </div>
+      <div className="section">
         <div className="card"> 
           <div className="card-content">
-            <div className="content">
+            <div className="content"> 
+              {/* {JSON.stringify(alphabets)} */}
+              {finalTime}<br/><br/>
+              {currAlphabet}<br/><br/>
             {alphabets.map((alphabet, i) => (
-                  <span key={i}>
+                  <>
                     <span>
                       {alphabet}
                       {/* {alphabet.split("").map((char, idx) => (
@@ -39,7 +98,7 @@ function App() {
                       )) } */}
                     </span>
                     <span> </span>
-                  </span>
+                  </>
                 ))}
 
             </div>
